8d7912d4ae23ff87c045103da5b31f7692025e30
alexandre-vpacheco <alexandre.v.pacheco@aluno.senai.br>
2023-10-14 20:05:27 -0300

    Ataulização logos

--- ReturnTrash/src/Components/DescartarButton.js
+++ ReturnTrash/src/Components/DescartarButton.js
@@ -8,7 +8,7 @@ const DescartarButton = ({ onpress }) => {
             onPress={onpress}>
             <View style={styles.button}>
                 <Image style={styles.logoDescarte} source={{
-                    uri: 'https://raw.githubusercontent.com/alexandre-vpacheco/app-ic-bj-front/f788fede015eab7590c9ffd34f770f4462c67d28/ReturnTrash/assets/logoDescarte.png',
+                    uri: 'https://raw.githubusercontent.com/alexandre-vpacheco/app-ic-bj-front/9a8674ef8d1f9639a6b3f86bdf9edecc0d52d97b/ReturnTrash/assets/logoDescarte.png',
                 }} />
 
                 <Text style={styles.buttonText}>Quero descartar!</Text>
